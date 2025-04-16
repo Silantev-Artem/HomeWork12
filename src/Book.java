460ce4e97f0e604fc00fafbbb1e93083fbd057a9
Silantev Artem <silantev.artem@yandex.ru>
2025-04-17 01:32:30 +0300

    Сделано дз №12

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String title;
+    private int publicationYear;
+    private Author author;
+
+    public Book(String title, int publicationYear, Author author) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+        Author carroll = new Author("Lewis", "Carroll");
+        Book alice = new Book("Alice through the Looking Glass", 2008, carroll);
+        System.out.println(carroll.getFirstName() + " " + carroll.getLastName() +  " -- " + alice.getTitle() + " , " + alice.getPublicationYear());
+
+        Author salinger = new Author("Jerome", "Salinger");
+        Book catcherRye = new Book("Catcer in the rye", 2010, salinger);
+        System.out.println(salinger.getFirstName() + " " + salinger.getLastName() + " -- " + catcherRye.getTitle() + " , " + catcherRye.getPublicationYear());
+
+        Author murakami = new Author("Haruki", "Murakami");
+        Book sputnik = new Book("Sputnik Sweetheart", 2001, murakami);
+        System.out.println(murakami.getFirstName() + " " + murakami.getLastName() + " -- " + sputnik.getTitle() + " , " + sputnik.getPublicationYear());
+
+        sputnik.setPublicationYear(2022);
+        System.out.println(murakami.getFirstName() + " " + murakami.getLastName() + " -- " + sputnik.getTitle() + " , " + sputnik.getPublicationYear());
+
+
+
     }
 }
